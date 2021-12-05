5f4ad3889bcf1bf2bb5a34f0726aec346bbc76f1
Dalci <bauer24j@gmail.com>
2021-12-06 01:43:36 +0900

    Modifying the basic layout and MVC Configuration
    - 레이아웃에서 이미지 헤더 제거
    - main 수정

---
+++ src/main/java/config/MvcConfig.java
@@ -0,0 +1,77 @@
+package config;
+
+import java.util.List;
+import java.util.Properties;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("main");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		//WebMvcConfigurer.super.addResourceHandlers(registry);
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
+		registry.addResourceHandler("/js/**").addResourceLocations("/resources/js");
+		registry.addResourceHandler("/img/**").addResourceLocations("/resources/img/");
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		registry.viewResolver(viewResolver);
+	}
+
+	@Override
+	public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+		SimpleMappingExceptionResolver seResolver = new SimpleMappingExceptionResolver();
+		//exception to view name mapping
+        Properties p = new Properties();
+        p.setProperty(NullPointerException.class.getName(), "npeView");
+        seResolver.addStatusCode("npeView", 404);
+		//exceptionResolver.set
+		
+		resolvers.add(seResolver);
+	}
+	
+	@Bean
+	public MultipartResolver multipartResolver() {
+		CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+		commonsMultipartResolver.setDefaultEncoding("UTF-8");
+		commonsMultipartResolver.setMaxUploadSize(104854600);
+		commonsMultipartResolver.setMaxInMemorySize(10485760);
+		return commonsMultipartResolver;
+	}
+	// 메시지
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
+		ms.setBasename("classpath:/messages");
+		ms.setDefaultEncoding("UTF-8");
+		return ms;
+	}
+}
