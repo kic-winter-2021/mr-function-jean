fe88e452fe1505c74bec50b1f58f1cc58549a755
Zion_1956 <Zion_1956@host.docker.internal>
2021-12-06 23:59:13 +0900

    21.12.06 추가작업

---
+++ src/main/java/controller/CenterController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("center")
+public class CenterController {
+	@GetMapping("question")
+	public String detail() {
+        System.out.println("center");
+		return ("center/question");
+	}
+
+}
