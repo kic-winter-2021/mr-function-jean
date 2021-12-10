577120754f42099b5108bbb581efd64980c313e6
Zion_1956 <Zion_1956@host.docker.internal>
2021-12-10 14:39:27 +0900

    .

---
+++ src/main/java/logic/dto/Question.java
@@ -0,0 +1,98 @@
+package logic.dto;
+
+import java.util.Date;
+
+import javax.validation.constraints.NotNull;
+
+public class Question {
+	public static int TYPE = 3; // 문의하기 타입
+	private int num;
+	@NotNull // input type="hidden"으로 넣어주기
+	private int type;
+	private String category;
+	private String title;
+	@NotNull
+	private String writer;
+	@NotNull(message = "내용을 입력하세요")
+	private String content;
+	private int ref;
+	private int qa;
+	private Date regDate;
+	private Date update;
+	private int views;	// 조회수
+	// 공지사항 type = 2
+	{ type=TYPE; qa=0; }//초기화 블록 생성자들을 생성하기 전에 실행됨.
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public int getRef() {
+		return ref;
+	}
+	public void setRef(int ref) {
+		this.ref = ref;
+	}
+	public int getQa() {
+		return qa;
+	}
+	public void setQa(int qa) {
+		this.qa = qa;
+	}
+	public Date getUpdate() {
+		return update;
+	}
+	public void setUpdate(Date update) {
+		this.update = update;
+	}
+	public int getViews() {
+		return views;
+	}
+	public void setViews(int views) {
+		this.views = views;
+	}
+	@Override
+	public String toString() {
+		return "Question [num=" + num + ", type=" + type + ", category=" + category + ", title=" + title + ", writer="
+				+ writer + ", content=" + content + ", ref=" + ref + ", qa=" + qa + ", regDate=" + regDate + ", update="
+				+ update + ", views=" + views + "]";
+	}
+	
+}
---
+++ src/main/java/logic/service/QuestionService.java
@@ -0,0 +1,29 @@
+package logic.service;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dao.BoardDao;
+import logic.dto.Question;
+
+@Service
+public class QuestionService {
+	@Autowired
+	BoardDao boardDao;
+	public int count() {
+		return boardDao.countQuestion();
+	}
+
+	public List<Question> list() {
+		return boardDao.listQuestion();
+	}
+
+	public void write(@Valid Question question) {
+		boardDao.writeQuestion(question);
+	}
+
+}
